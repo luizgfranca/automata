87879c2d44bdee7ab7ab748033b2db45eec57b79
luizgfc <luizgfc@proton.me>
2025-11-21 11:54:18 -0300

    fix suggestion navigation

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,22 @@
-mod suggestions;
-mod sysinfo;
-mod sysaction;
-mod sessionmgr;
-mod fsutil;
 mod component;
+mod fsutil;
+mod mathutils;
+mod sessionmgr;
+mod suggestions;
+mod sysaction;
+mod sysinfo;
 
 use std::sync::{Arc, Mutex};
 
 use component::suggestion_row::{SuggestionRow, SuggestionRowData};
 use gtk4::gio::{self, AppInfo};
+use mathutils::*;
 use suggestions::SuggestionMgr;
 
 use gtk::prelude::*;
 use gtk::{Application, ApplicationWindow, glib};
 use gtk4::gdk::Key;
-use gtk4::{self as gtk, gdk, EventControllerKey, ScrolledWindow};
+use gtk4::{self as gtk, EventControllerKey, ScrolledWindow, gdk};
 
 fn load_css() {
     let display = gdk::Display::default().expect("unable to load default display");
@@ -47,27 +49,26 @@ fn main() -> glib::ExitCode {
             window.present();
             return;
         }
-        
+
         let window = ApplicationWindow::builder()
-                .application(app)
-                .default_width(1100)
-                .default_height(600)
-                .title("Hello, World!")
-                .decorated(false)
-                .build();
+            .application(app)
+            .default_width(1100)
+            .default_height(600)
+            .title("Hello, World!")
+            .decorated(false)
+            .build();
 
         load_css();
 
         let main_input = gtk::Entry::new();
         main_input.add_css_class("main-input");
-        
+
         let list_store = gio::ListStore::new::<SuggestionRowData>();
         let factory = gtk::SignalListItemFactory::new();
         factory.connect_setup(move |_factory, item| {
             let row = SuggestionRow::default();
             item.set_child(Some(&row));
         });
-
 
         factory.connect_bind(move |_factory, item| {
             let item = item.downcast_ref::<gtk::ListItem>().unwrap();
@@ -88,10 +89,10 @@ fn main() -> glib::ExitCode {
                 // let label = gtk::Label::new(Some(&it.title));
                 // suggestion_list_ui.append(&label);
                 list_store.append(&SuggestionRowData::new(
-                    &it.id, 
+                    &it.id,
                     &it.title,
                     &it.description,
-                    it.icon_path.clone()
+                    it.icon_path.clone(),
                 ));
             }
         }
@@ -107,29 +108,31 @@ fn main() -> glib::ExitCode {
 
             mgr.update(&input_str);
             list_store_clone.remove_all();
-            
+
             for it in mgr.get_suggestions() {
                 list_store_clone.append(&SuggestionRowData::new(
-                    &it.id, 
-                    &it.title, 
+                    &it.id,
+                    &it.title,
                     &it.description,
-                    it.icon_path.clone()
+                    it.icon_path.clone(),
                 ));
             }
         });
 
         let selection_model = gtk::SingleSelection::new(Some(list_store));
-        let list_view = gtk::ListView::new(Some(selection_model), Some(factory));
+        let list_view = gtk::ListView::new(Some(selection_model.clone()), Some(factory));
         let suggestion_mgr_clone = suggestion_mgr.clone();
         list_view.connect_activate(move |list_view, position| {
             let model = list_view.model().unwrap();
-            let row_data = model.item(position).and_downcast::<SuggestionRowData>().unwrap();
+            let row_data = model
+                .item(position)
+                .and_downcast::<SuggestionRowData>()
+                .unwrap();
             {
                 let mgr = suggestion_mgr_clone.lock().expect("SuggestionMgr poisoned");
                 mgr.run_by_id(&row_data.id());
-            }    
+            }
         });
-
 
         let suggestion_list_scrollable = ScrolledWindow::builder()
             .hscrollbar_policy(gtk::PolicyType::Never)
@@ -151,15 +154,14 @@ fn main() -> glib::ExitCode {
                 .lock()
                 .expect("unable to get suggestion list lock");
 
-            let selected = mgr.get_suggestions()
+            let selected = mgr
+                .get_suggestions()
                 .get(idx)
                 .expect("suggestion selected index not found on list");
 
             mgr.run(&selected);
             window_clone.close();
         });
-
-
 
         let window_clone = window.clone();
         let suggestion_mgr_clone = suggestion_mgr.clone();
@@ -171,7 +173,8 @@ fn main() -> glib::ExitCode {
                 .lock()
                 .expect("unable to get suggestion list lock");
 
-            let selected = mgr.get_suggestions()
+            let selected = mgr
+                .get_suggestions()
                 .get(idx)
                 .expect("suggestion selected index not found on list");
 
@@ -184,6 +187,7 @@ fn main() -> glib::ExitCode {
         let suggestion_list_ui_clone = suggestion_list_ui.clone();
         let suggestion_mgr_clone = suggestion_mgr.clone();
         let main_input_clone = main_input.clone();
+        let selection_model_clone = selection_model.clone();
         key_controller.connect_key_pressed(move |_, key, _, _| {
             dbg!("key_controller.connect_key_pressed");
             dbg!(&key);
@@ -192,10 +196,10 @@ fn main() -> glib::ExitCode {
                 Key::Tab => {
                     let idx: usize = match suggestion_list_ui_clone.selected_row() {
                         Some(current) => current.index().try_into().unwrap(),
-                        None => 0
+                        None => 0,
                     };
 
-                    // this is done to avoid a deadlock when on_changed on the main 
+                    // this is done to avoid a deadlock when on_changed on the main
                     // input is triggered because both lock the suggestionMgr
                     // TODO: i reeeeally should organize my ownership structure to
                     //       manage this lock better
@@ -209,7 +213,7 @@ fn main() -> glib::ExitCode {
                             .expect("suggestion selected index not found on list")
                             .clone()
                     };
-                   
+
                     if let Some(completion) = &selected.completion {
                         dbg!(&completion);
                         main_input_clone.set_text(completion);
@@ -220,14 +224,15 @@ fn main() -> glib::ExitCode {
                 Key::Return => {
                     let idx: usize = match suggestion_list_ui_clone.selected_row() {
                         Some(current) => current.index().try_into().unwrap(),
-                        None => 0
+                        None => 0,
                     };
 
                     let mgr = suggestion_mgr_clone
                         .lock()
                         .expect("unable to get suggestion list lock");
 
-                    let selected = mgr.get_suggestions()
+                    let selected = mgr
+                        .get_suggestions()
                         .get(idx)
                         .expect("suggestion selected index not found on list");
 
@@ -236,26 +241,24 @@ fn main() -> glib::ExitCode {
                     return gtk::glib::Propagation::Stop;
                 }
                 Key::Down => {
-                    if let Some(current) = suggestion_list_ui_clone.selected_row() {
-                        let next_index = current.index() + 1;
-                        if let Some(next_row) = suggestion_list_ui_clone.row_at_index(next_index) {
-                            suggestion_list_ui_clone.select_row(Some(&next_row));
-                        }
-                    } else {
-                        if let Some(first) = suggestion_list_ui_clone.row_at_index(0) {
-                            suggestion_list_ui_clone.select_row(Some(&first));
-                        }
-                    }
+                    let new_position = u32_increment_wrap(
+                        selection_model_clone.selected(),
+                        0,
+                        selection_model_clone.n_items() - 1,
+                    );
+                    dbg!((selection_model_clone.selected(), &new_position));
+                    selection_model_clone.set_selected(new_position);
+                    return gtk::glib::Propagation::Stop;
                 }
                 Key::Up => {
-                    if let Some(current) = suggestion_list_ui_clone.selected_row() {
-                        let prev_index = current.index() - 1;
-                        if prev_index >= 0 {
-                            if let Some(prev_row) = suggestion_list_ui_clone.row_at_index(prev_index) {
-                                suggestion_list_ui_clone.select_row(Some(&prev_row));
-                            }
-                        }
-                    }
+                    let new_position = u32_decrement_wrap(
+                        selection_model_clone.selected(),
+                        0,
+                        selection_model_clone.n_items() - 1,
+                    );
+                    dbg!((selection_model_clone.selected(), &new_position));
+                    selection_model_clone.set_selected(new_position);
+                    return gtk::glib::Propagation::Stop;
                 }
                 _ => (),
             };
